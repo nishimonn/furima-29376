a208b28c7928e916c0d32cac566e91e7d72cc778
nishimonn <n.toppo.s.n@gmail.com>
2020-09-11 16:28:19 +0900

    バリデーション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
+  end
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nick_name, :family_name, :first_name, :birthday presence: 
+    validates :email, uniqueness: 
+    validates :email, inclusion: { in: %w(draft publish private) }
+    validates :password, length: { minimum: 6 }
+    validates :password, hankakueisuu:
+    validates :password, nikainyuuryoku:
+    validates :family_name, :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :family_name, :first_name, hurugana:
+    #validates :name_reading, format:{with: /\A[ァ-ヶー－]+\z/}
+  end
+
 end
