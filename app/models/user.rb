409537ba0be4c02d12b78e08e517b704777d1f3b
nishimonn <n.toppo.s.n@gmail.com>
2020-09-15 19:10:32 +0900

    画面遷移修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
     with_options presence: true do
       validates :nick_name, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday#ニックネーム、苗字、名前、生年月日は必須
     end
-    validates :password, format: {with: /\A[a-zA-Z0-9]+\z/} #パスワードは半角英数字。
+    # validates :password, format: {with: /\A[a-zA-Z0-9]+\z/} #パスワードは半角英数字。
     validates :family_name, :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}#本名は全角（漢字・ひらがな・カタカナ）で入力
-    validates :family_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}#本名のフリガナの表現がわからない
+    validates :family_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
     year_month_day = /\A\d{4}-\d{2}-\d{2}\z/
     validates :birthday, presence: true, format: { with: year_month_day }
 end
