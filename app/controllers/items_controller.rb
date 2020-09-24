3b214dd5bf1cbc713ef08cfaeb7f4c6d396b148f
nishimonn <n.toppo.s.n@gmail.com>
2020-09-24 15:55:03 +0900

    インデント修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,10 +28,10 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if  @item.update(item_params)
-    redirect_to item_path(@item)
+    if @item.update(item_params)
+      redirect_to item_path(@item)
     else
-    render :edit
+      render :edit
     end
   end
 
