fa60449fc562b291f3301c1b795a8e5c5cbbe3cd
nishimonn <n.toppo.s.n@gmail.com>
2020-09-24 15:15:26 +0900

    updateアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,8 +30,9 @@ class ItemsController < ApplicationController
   def update
     if  @item.update(item_params)
     redirect_to item_path(@item)
-    end
+    else
     render :edit
+    end
   end
 
   private
