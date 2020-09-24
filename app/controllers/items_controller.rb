30e40f95481f7fa0d881d0a9eaaa2d579d1972d7
nishimonn <n.toppo.s.n@gmail.com>
2020-09-24 15:11:22 +0900

    エラーハンドリング表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,8 +28,10 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
+    if  @item.update(item_params)
     redirect_to item_path(@item)
+    end
+    render :edit
   end
 
   private
