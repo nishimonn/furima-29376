156151c4d381944c61d87feabb4526f361baddac
nishimonn <n.toppo.s.n@gmail.com>
2020-09-28 13:56:47 +0900

    buyとshipping_addressのマイグレーションファイル編集

--- db/migrate/20200928042318_create_shipping_addresses.rb
+++ db/migrate/20200928042318_create_shipping_addresses.rb
@@ -1,6 +1,13 @@
 class CreateShippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_addresses do |t|
+      t.string :postal_code,      null: false
+      t.integer :prefectures_id,  nill: false
+      t.string :city,             null: false
+      t.string :address,          null: false
+      t.string :building_name     default: ""
+      t.string :phone_number,     nill: false
+      t.references :buy,          null: false, foreign_key: true
 
       t.timestamps
     end
